59bdd5b3d7208bedc61739424db8899531e49493
Gasper Kojek <gasper.kojej@gmail.com>
2020-05-26 19:10:54 +0200

    WIP 15: @Provides

--- app/src/main/java/com/example/android/dagger/di/StorageModule.kt
+++ app/src/main/java/com/example/android/dagger/di/StorageModule.kt
@@ -1,16 +1,31 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.storage.SharedPreferencesStorage
 import com.example.android.dagger.storage.Storage
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 
 // Tells Dagger this is a Dagger module
 // Because of @Binds, StorageModule needs to be an abstract class
 @Module
-abstract class StorageModule {
+/*abstract*/ class StorageModule {
 
     // Makes Dagger provide SharedPreferencesStorage when a Storage type is requested
-    @Binds
-    abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+//    @Binds
+//    abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+
+    // @Provides tell Dagger how to create instances of the type that this function
+    // returns (i.e. Storage).
+    // Function parameters are the dependencies of this type (i.e. Context).
+    @Provides
+    fun provideStorage(context: Context): Storage {
+        // Whenever Dagger needs to provide an instance of type Storage,
+        // this code (the one inside the @Provides method) will be run.
+
+        // Useful for when we're referencing code not owned by us, for instance Retrofit,
+        // otherwise it's better to use @Binds
+        return SharedPreferencesStorage(context)
+    }
 }
